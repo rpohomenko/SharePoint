a1140ec94389d39a73720a47ca900be286132e1e
rpohomenko <rpohomenko@hotmail.com>
2019-10-18 17:25:54 +0300

    dynamic linq

--- Samples/SP.ProjectTask/SP.ProjectTaskWeb/Controllers/WebController.cs
+++ Samples/SP.ProjectTask/SP.ProjectTaskWeb/Controllers/WebController.cs
@@ -1,8 +1,12 @@
-﻿using SharePoint.Authentication;
+﻿using Microsoft.SharePoint.Client;
+using SharePoint.Authentication;
 using SharePoint.Authentication.Owin.Extensions;
+using SP.Client.Linq;
 using SP.ProjectTaskWeb.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Linq.Dynamic.Core;
 
 namespace SP.ProjectTaskWeb.Controllers
 {
@@ -17,30 +21,112 @@ namespace SP.ProjectTaskWeb.Controllers
             _tokenHelper = lowTrustTokenHelper;
         }
 
-        // GET api/web/tasks/{id}
         [Route("tasks/{id}")]
         [HttpGet]
-        public ProjectTask GetProjectTask(int id)
+        public IHttpActionResult GetProjectTask(int id)
         {
-            using (var context = _tokenHelper.CreateClientContext())
+            ProjectTask item = GetItem<ProjectTask>(id);
+            return Json(item);
+        }
+
+        [Route("tasks")]
+        [HttpGet]
+        public IHttpActionResult GetProjectTasks([FromUri] string where = null, [FromUri] int count = 0, [FromUri] string sortBy = null, [FromUri] bool sortDesc = false)
+        {
+            IEnumerable<ProjectTask> items = GetItems<ProjectTask>(where, count, sortBy, sortDesc);
+            return Json(items);
+        }
+
+        [Route("projects/{id}")]
+        [HttpGet]
+        public IHttpActionResult GetProject(int id)
+        {
+            Project item = GetItem<Project>(id);
+            return Json(item);
+        }
+
+        [Route("projects")]
+        [HttpGet]
+        public IHttpActionResult GetProjects([FromUri] string where = null, [FromUri] int count = 0, [FromUri] string sortBy = null, [FromUri] bool sortDesc = false)
+        {
+            IEnumerable<Project> items = GetItems<Project>(where, count, sortBy, sortDesc);
+            return Json(items);
+        }
+
+        [Route("employees/{id}")]
+        [HttpGet]
+        public IHttpActionResult GetEmployee(int id)
+        {
+            Employee item = GetItem<Employee>(id);
+            return Json(item);
+        }
+
+        [Route("employees")]
+        [HttpGet]
+        public IHttpActionResult GetEmployees([FromUri] string where = null, [FromUri] int count = 0, [FromUri] string sortBy = null, [FromUri] bool sortDesc = false)
+        {
+            IEnumerable<Employee> items = GetItems<Employee>(where, count, sortBy, sortDesc);
+            return Json(items);
+        }
+
+        [Route("departments/{id}")]
+        [HttpGet]
+        public IHttpActionResult GetDepartment(int id)
+        {
+            Department item = GetItem<Department>(id);
+            return Json(item);
+        }
+
+        [Route("departments")]
+        [HttpGet]
+        public IHttpActionResult GetDepartments([FromUri] string where = null, [FromUri] int count = 0, [FromUri] string sortBy = null, [FromUri] bool sortDesc = false)
+        {
+            IEnumerable<Department> items = GetItems<Department>(where, count, sortBy, sortDesc);
+            return Json(items);
+        }
+
+        private TEntity GetItem<TEntity>(int id) where TEntity : ListItemEntity, new()
+        {
+            using (ClientContext context = _tokenHelper.CreateClientContext())
             {
-                var ctx = new ProjectTaskContext(context);
-                return id > 0 ? ctx.List<ProjectTask>().FirstOrDefault(item => item.Id == id) : null;
+                ProjectTaskContext projectTaskContext = new ProjectTaskContext(context);
+                return (id > 0) ? projectTaskContext.List<TEntity>().FirstOrDefault((TEntity item) => item.Id == id) : null;
             }
         }
 
-        // POST api/web/deploy
+        private IEnumerable<TEntity> GetItems<TEntity>(string where = null, int count = 0, string sortBy = null, bool sortDesc = false) where TEntity : ListItemEntity, new()
+        {
+            using (ClientContext context = _tokenHelper.CreateClientContext())
+            {
+                ProjectTaskContext projectTaskContext = new ProjectTaskContext(context);
+                IQueryable<TEntity> source = projectTaskContext.List<TEntity>();
+                if (!string.IsNullOrWhiteSpace(where))
+                {
+                    source = source.Where(where);
+                }
+                if (!string.IsNullOrWhiteSpace(sortBy))
+                {
+                    source = source.OrderBy(sortDesc ? (sortBy + " DESC") : sortBy);
+                }
+                if (count <= 0 || count > 10000)
+                {
+                    count = 100;
+                }
+                source = source.Take(count);
+                return source.ToArray();
+            }
+        }
+
         [Route("deploy")]
-        //[HttpGet]
         [HttpPost]
         public void Deploy()
         {
-            using (var context = _tokenHelper.CreateClientContext())
+            using (ClientContext context = _tokenHelper.CreateClientContext())
             {
-                var ctx = new ProjectTaskContext(context);
-                var model = ctx.CreateModel();
-                model.UnProvision();
-                model.Provision();
+                ProjectTaskContext projectTaskContext = new ProjectTaskContext(context);
+                ProjectTaskProvisionModel<SpDataContext> projectTaskProvisionModel = projectTaskContext.CreateModel();
+                projectTaskProvisionModel.UnProvision();
+                projectTaskProvisionModel.Provision();
             }
         }
     }
